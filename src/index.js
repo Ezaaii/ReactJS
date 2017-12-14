79dfff190891c7bf5faf104888ad86a53022e20e
Unknown <n.vanroost@gmail.com>
2017-12-14 12:20:47 +0100

    JE OUI (j'ai juste enfin compris pourquoi ça s'affichait pas)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Welcome from './Welcome';
+import Hello from './Hello';
 
 ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));
